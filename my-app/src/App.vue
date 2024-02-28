180774acaacd3a3938b460a499432eedd4f14eef
dodsoneatwit <dodsone@wit.edu>
2024-02-27 20:49:27 -0500

    added toolbar and navbars to top of window

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <v-card>
+        <NavigationBar />
+        <v-tabs 
+          v-model="tab" 
+          bg-color="red"
+          color="black"
+          fixed-tabs
+          align-tabs="center"
+        >
+          <v-tab value="one">Gallery</v-tab>
+          <v-tab value="two">Generate</v-tab>
+        </v-tabs>
+      </v-card>
     </v-main>
   </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+import NavigationBar from "./components/NavigationBar.vue";
+
+export default {
+  name: "App",
+  data: () => ({
+  }),
+  components: {
+    NavigationBar,
+  },
+};
 </script>
---
+++ my-app/src/components/NavigationBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-toolbar 
+      density="comfortable"
+      color='red'
+      dark
+    >
+        <v-toolbar-title class="flex text-center">RECIPE ROULETTE</v-toolbar-title>
+        <v-btn icon>
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavigationBar',
+  data: () => ({
+  }),
+  components: {
+  },
+  created: function () {
+  },
+  methods: {
+  }
+}
+  //
+</script>
